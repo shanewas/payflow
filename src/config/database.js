8425310520392b698cb912669cc004ec73ccff21
Shanewas Ahmed <shanewasahmed@gmail.com>
2020-01-22 11:23:45 +0900

    feat: add PostgreSQL connection with pg pool

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  close: () => {
+    console.log('Closing database connection pool');
+    return pool.end();
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
 require('dotenv').config();
 
 const app = require('./app');
+const db = require('./config/database');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const gracefulShutdown = () => {
+  console.log('Received kill signal, shutting down gracefully.');
+  server.close(() => {
+    console.log('Closed out remaining connections.');
+    db.close().then(() => {
+       console.log('Database connection closed.');
+       process.exit(0);
+    });
+  });
+
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 10000);
+};
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
