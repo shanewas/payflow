95c9e688a091bd856b640dbb31a28277856bf1f2
Shanewas Ahmed <shanewasahmed@gmail.com>
2020-04-21 13:50:43 +0900

    feat: initialize Stripe SDK configuration

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,41 +1,31 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+require('dotenv').config();
+const Stripe = require('stripe');
 
-/**
- * A helper function to classify Stripe errors.
- * @param {Error} error The error object from a Stripe API call.
- * @returns {string} A string representing the type of error.
- */
-function classifyStripeError(error) {
-  if (!error.type) {
-    return 'generic_error';
-  }
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2020-08-27', // Use a specific API version for consistency
+    typescript: false,
+});
 
-  switch (error.type) {
-    case 'StripeCardError':
-      // A declined card error
-      return 'card_error';
-    case 'StripeRateLimitError':
-      // Too many requests made to the API too quickly
-      return 'rate_limit_error';
-    case 'StripeInvalidRequestError':
-      // Invalid parameters were supplied to Stripe's API
-      return 'invalid_request_error';
-    case 'StripeAPIError':
-      // An error occurred internally with Stripe's API
-      return 'api_error';
-    case 'StripeConnectionError':
-      // Some kind of error occurred during the HTTPS communication
-      return 'connection_error';
-    case 'StripeAuthenticationError':
-      // You probably used an incorrect API key
-      return 'authentication_error';
-    default:
-      // Handle other types of unexpected errors
-      return 'unknown_error';
-  }
-}
+const isStripeError = (error) => {
+    return error.type && error.type.startsWith('Stripe');
+};
+
+const classifyStripeError = (error) => {
+    if (!isStripeError(error)) {
+        return {
+            type: 'Unknown',
+            message: 'An unexpected error occurred.',
+        };
+    }
+    return {
+        type: error.type,
+        message: error.message,
+        statusCode: error.statusCode,
+    };
+};
 
 module.exports = {
-  stripe,
-  classifyStripeError,
+    stripe,
+    isStripeError,
+    classifyStripeError,
 };
