644f998f008057c41cab2ba589f4f949299437fa
Shanewas Ahmed <shanewasahmed@gmail.com>
2021-07-02 10:00:00 +0900

    fix: configure API client to use REACT_APP_API_URL environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
 });
 
 // Add a request interceptor to include the token in headers
